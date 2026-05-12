c38dcf4722e8306d289b98d0ab67007638f755fb
huke <qq854734279@163.com>
2026-05-12 12:41:21 +0800

    Initial commit: empty project scaffold

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        // 添加镜像源
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        // 保留官方源（作为备用）
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MnnLlmChat"
+include(":app")
+ 
